0e547c523dfe23e8579a5873fd0ee8d79b311a18
Arun21102003 <arun.srinivasan@fibonalabs.com>
2024-06-12 00:16:16 +0530

    AWS

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "aws.cnssscm8oue1.us-east-2.rds.amazonaws.com"
+customuser = "root"
+custompass = "Arunsr2003"
+customdb = "aws"
+custombucket = "myawsbucket212003"
+customregion = "us-east-2"
